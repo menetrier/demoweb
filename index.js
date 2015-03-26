0146d04dc631ef5ec3c81c6813367252092337a0
Jeremy Menetrier (Selectra) <jeremy.menetrier@gmail.com>
2015-03-26 10:30:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var http = require('http');
+var port = process.env.PORT || 8000;
+
+http.createServer(function(request, response) {
+  console.log('new request !');
+
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(port, function() {
+  console.log('Server started on port', port);
+});
+
+
